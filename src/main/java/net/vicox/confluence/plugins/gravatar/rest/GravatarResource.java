c20563aabef8513f7c15784ae72c624727798458
Georg Schmidl <georg.schmidl@vicox.net>
2015-02-07 17:35:14 +0100

    add import call to rest api

--- src/main/java/net/vicox/confluence/plugins/gravatar/rest/GravatarResource.java
+++ src/main/java/net/vicox/confluence/plugins/gravatar/rest/GravatarResource.java
@@ -3,8 +3,8 @@ package net.vicox.confluence.plugins.gravatar.rest;
 import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 import com.atlassian.user.User;
-import net.vicox.confluence.plugins.gravatar.util.GravatarUtil;
 import net.vicox.confluence.plugins.gravatar.service.GravatarImportService;
+import net.vicox.confluence.plugins.gravatar.util.GravatarUtil;
 import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,21 +14,23 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 /**
- * Resource for getting {@link GravatarInfo}s.
+ * Resource for getting {@link net.vicox.confluence.plugins.gravatar.rest.GravatarInfo}s.
  *
  * @author Georg Schmidl
  */
-@Path("/info")
-public class GravatarInfoResource {
+@Path("/")
+public class GravatarResource {
 
     private GravatarImportService gravatarImportService;
 
     @GET
     @AnonymousAllowed
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getUserInfo(@Context HttpServletRequest request) {
+    @Path("info")
+    public Response getGravatarInfo(@Context HttpServletRequest request) {
         GravatarInfo gravatarInfo = new GravatarInfo();
 
         User user = AuthenticatedUserThreadLocal.get();
@@ -42,6 +44,20 @@ public class GravatarInfoResource {
         }
 
         return Response.ok(gravatarInfo).build();
+    }
+
+    @GET
+    @AnonymousAllowed
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("import")
+    public Response importGravatar(@Context HttpServletRequest request) {
+        User user = AuthenticatedUserThreadLocal.get();
+        try {
+            gravatarImportService.importGravatar(user);
+            return getGravatarInfo(request);
+        } catch (IOException e) {
+            return Response.serverError().build();
+        }
     }
 
     public void setGravatarImportService(GravatarImportService gravatarImportService) {
